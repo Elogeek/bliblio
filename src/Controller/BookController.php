681e79a205060d42f967c5f3b1b31f3ec8ec6037
elogeek <elodiechristin@gmail.com>
2022-02-25 12:06:39 +0100

    suite project

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -16,15 +16,22 @@ class BookController extends AbstractController {
         $this->em = $em;
     }
 
-    // Add product in the db
+    /**
+     * Add product in the db
+     */
     #[Route('/add', name: 'add')]
     public function addProduct(EntityManagerInterface $em) : Response {
-        $b = new Book();
-        $b
+        $book = new Book();
+        $book
             ->setName('super nom')
+            ->setCategory()
+            ->setAvailable(1)
+            ->setReader('')
+            ->setAuthor('bibi')
+            ->setShortDescription('une super description')
         ;
 
-        $em->persist($b);
+        $em->persist($book);
 
         // Enregistrement en dbb
         $em->flush();
@@ -32,31 +39,48 @@ class BookController extends AbstractController {
         return $this->render('book/add.html.twig');
     }
 
-    //Update a product
+    /**
+     * Update a product
+     */
     #[Route('/update/{id}', name: 'update')]
-    public function updateBook(Book $b, EntityManagerInterface $em): Response {
-        $b->setName('Mon');
+    public function updateBook(Book $book, EntityManagerInterface $em): Response {
+        $book->setName('Mon livre');
 
         $em->flush();
 
         return $this->render('book/update.html.twig');
     }
 
-    //Delete a product
+    /**
+     * Delete a product
+     */
     #[Route('/delete/{id}', name: 'delete')]
     public function deleteProduct(Book $book, EntityManagerInterface $em): Response {
         $em->remove($book);
         $em->flush();
-        return $this->render('book/delete.htm.twig');
+        return $this->render('book/delete.html.twig');
     }
 
-    // Read
-    #[Route('/view/first', name: 'view_first')]
-    public function viewSingleBook(EntityManagerInterface $em): Response {
-        $bookRepository = $em->getRepository(Book::class);
-        $b = $bookRepository->find(1);
-        return $this->render('book\home.html.twig', [
-            'product' => $b,
+    /**
+     * Read a single book
+     */
+    #[Route('/view/{id}', name: 'view_first')]
+    public function viewSingleBook(Book $book): Response {
+
+        return $this->render('book/single.html.twig', [
+            'book' => $book,
         ]);
     }
+
+    /**
+     * Read all book
+     */
+    #[Route('view/all', name: 'view-all')]
+    public function viewAllBooks(EntityManagerInterface $em): Response {
+        $books = $em->getRepository(Book::class)->findAll();
+        return $this->render('book/home.html.twig', [
+            'books' => $books,
+        ]);
+    }
+
 }
